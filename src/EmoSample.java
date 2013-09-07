20730a5e3a659ffbed1c7d6336985a5aa22c7ba7
Alon Lavi <alonlavi@gmail.com>
2013-09-06 21:53:24 -0400

    emo sample fields

--- src/EmoSample.java
+++ src/EmoSample.java
@@ -1,11 +1,31 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 public class EmoSample {
 
+	@JsonProperty("local_time")
 	int localTime;
 	
+	@JsonProperty("connection_strength")
 	float connectionStrength;
 	
+	@JsonProperty("meditation")
+	float meditation;
+	@JsonProperty("engagement")
+	float engagement;
+	@JsonProperty("happiness")
+	float happiness;
+	@JsonProperty("excitement")
+	float excitement;
 	
+	@JsonProperty("wink_left")
+	boolean winkLeft;
+	@JsonProperty("wink_right")
+	boolean winkRight;
 	
+	@JsonProperty("turn_x")
+	int turnX;
+	@JsonProperty("turn_y")
+	int turnY;
 	
 }
